a51cb04578334f24bbeb3e8902d197affc25f9b4
Giancarlo Cicellyn Comneno <giancarlo.cicellyn@gmail.com>
2024-10-01 21:02:01 +0200

    various fixing

---
+++ go.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script per l'aggiornamento del database in base all'analisi di più estrazioni
+echo "Inizio esecuzione: $(date)"
+python.exe I:\\DATA\\MyWorkspace\\Progetti\\giancolotto\\src\\main.py
+echo "Fine esecuzione: $(date)"
