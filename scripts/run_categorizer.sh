83d80df6660fef7b788c89c9f15240da5d6dc662
Giancarlo Cicellyn Comneno <giancarlo.cicellyn@gmail.com>
2025-01-29 12:14:48 +0100

    Fixing debugging log on run_categorizer.sh

--- scripts/run_categorizer.sh
+++ scripts/run_categorizer.sh
@@ -57,7 +57,7 @@ fi
 # Loop per eseguire lo script Python senza parametri DIFFERENZA volte
 for ((i = 0; i < DIFFERENZA; i++)); do
     sed -i "s/^offset_estr=.*/offset_estr=${i}/" "$config_file"
-    echo "Esecuzione ${i+1} di $DIFFERENZA"
+    echo "Esecuzione offset ${i} di $((DIFFERENZA - 1))"
     python.exe ./src/utils/cifrolotto_categorizer.py --output-file "$OUTPUT_FILE"
     if [ $? -ne 0 ]; then
         echo "Errore durante l'esecuzione dello script Python nella iterazione $i. Terminazione."
