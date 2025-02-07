8b10444cc43c7707d8d86d0b70320c8a5e6a0bef
Giancarlo Cicellyn Comneno <giancarlo.cicellyn@gmail.com>
2025-02-07 09:13:43 +0100

    Parameterizing run_all_categorizer.sh

--- scripts/run_all_categorizer.sh
+++ scripts/run_all_categorizer.sh
@@ -10,28 +10,36 @@ script_root="$(realpath "${script_dir}")"
 script_file="${script_root}/run_categorizer.sh"
 
 # Imposta i valori di default per gli offset, numero di estrazioni
+OFFSET_INIZIO=0
+OFFSET_FINE=0
 GROUP_OFFSET_INIZIO=0
 GROUP_OFFSET_FINE=0
 
 # Leggi i parametri in ingresso
 if [ "$#" -ge 1 ]; then
-    GROUP_OFFSET_INIZIO=$1
+    OFFSET_INIZIO=$1
 fi
 if [ "$#" -ge 2 ]; then
-    GROUP_OFFSET_FINE=$2
+    OFFSET_FINE=$2
+fi
+
+if [ "$#" -ge 3 ]; then
+    GROUP_OFFSET_INIZIO=$3
+fi
+if [ "$#" -ge 4 ]; then
+    GROUP_OFFSET_FINE=$4
 fi
 
 ANNO=$(date +"%Y") # Imposta il valore di default all'anno corrente
-if [ "$#" -ge 3 ]; then
-    ANNO=$3
+if [ "$#" -ge 5 ]; then
+    ANNO=$5
 fi
 
 # Loop per eseguire lo script con diversi offset
 for ((i=GROUP_OFFSET_INIZIO; i<=GROUP_OFFSET_FINE; i++)); do
-    echo "[DEBUG] Dataset svuotato per la prossima esecuzione"
     > ./dataset/cifrolotto.data
 
     # Esegue lo script e filtra solo i log che iniziano con [INFO]
-    echo "[INFO] Esecuzione da offset 0 a 9, con gruppo da $i estrazioni: anno $ANNO"
-    bash "${script_file}" 0 200 "$i" "$ANNO" | grep "^\[INFO\]"    
+    echo "[INFO] Esecuzione da offset $OFFSET_INIZIO a $OFFSET_FINE, con gruppo da $i estrazioni: anno $ANNO"
+    bash "${script_file}" "$OFFSET_INIZIO" "$OFFSET_FINE" "$i" "$ANNO" | grep "^\[INFO\]"
 done
